a5503651924460e84c671031b6ec291f1f5f26a9
Alexis-Valdes <avaldesg@ucsc.edu>
2024-01-09 20:01:55 -0800

    Promts for week 1
    Session1 and Session 2

---
+++ Session1.py
@@ -0,0 +1,10 @@
+# Name: Alexis Valdes
+# Date: 1/9/24
+# Write a Python program to print out your full name,
+# your preferred pronouns (optional), and two sentences
+# about your favorite movie and your favorite food.
+
+print('''Alexis Valdes, He/Him
+I dont have a favorite movie but my favorite show is supernatural.
+My favorite food has to be hands down some seafood boil specifically boiling crab.
+''')
---
+++ Session2.py
@@ -0,0 +1,21 @@
+# Name: Alexis Valdes
+# Date: 1/9/24
+# Write a Python program that prints the sum of two floating point numbers,
+# the difference between two integers, and the product of a floating point
+# number and an integer. In each case, have the program print out the data
+# type of the resulting answer.
+
+a = 4.0
+b = 5.2
+print(f"The sum of {a} and {b} = {a + b}")
+print(f"The type is of {type(a+b)}")
+
+a = 3
+b = 6
+print(f"\nThe difference between {a} and {b} is {b-a}")
+print(f"The type is of {type(a+b)}")
+
+a = 7.8
+b = 10
+print(f"\n The product of {a} and {b} is {a*b}")
+print(f"The type is of {type(a+b)}")
