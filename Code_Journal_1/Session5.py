30ea6bbb200c4c71d2ace6cd41881f9ed3c49189
Alexis-Valdes <avaldesg@ucsc.edu>
2024-01-29 12:24:28 -0800

    updated session 5 to the correct tables

--- Code_Journal_1/Session5.py
+++ Code_Journal_1/Session5.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from tabulate import tabulate
 # Write a Python program that writes out a table of the function sin(x)
 # vs. x, where x is tabulated between 0 and 2 with a thousand entries.
 # Follow the basic Python program structure, including a main program
@@ -7,10 +7,16 @@ import numpy as np
 
 def main():
     x_values = np.arange(0, 2.002,  2/1000)
+    sin_list = []
 
-    print('|   x   |    sin(x)   |   y   |')
     for x in x_values:
-        print(f"|   {x}    |   sin({x})    |   {np.sin(x):12}  |")
+        sin_list.append((x, np.sin(x)))
+
+
+
+    table_headers = [f"sin(x)", "y"]
+    print(tabulate(sin_list, headers=table_headers, floatfmt=".3f"))
+
 
 
 if __name__ == "__main__":
